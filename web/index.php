c465d8fee76d302946de37301a7dc7d3d414c192
Kelly Remnant <remnankell@AD.ECS.VUW.AC.NZ>
2018-11-13 12:05:10 +1300

    10

--- web/index.php
+++ web/index.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Extinct Test</title>
+    <link rel="stylesheet" href="phpTest.css">
+</head>
+<body>
 <?php
     $db = parse_url(getenv("DATABASE_URL"));
     $dbconnect=mysqli_connect($db);
@@ -7,5 +17,35 @@
     } else {
         echo "Connected!";
     }
+
+
 ?>
 <p>This is the main page</p>
+<table>
+    <tr>
+    <td><b>Name</b></td>
+    <td><b>Scientific Name</b></td>
+    <td><b>Information</b></td>
+    </tr>
+
+<?php
+
+$query = mysqli_query($dbconnect, "SELECT * FROM recipes")
+or die (mysqli_error($dbconnect));
+
+while ($row = mysqli_fetch_array($query)) {
+echo
+"<tr>
+    <td><img src=".$row['burgername']." ></td>
+    <td>{$row['burgerbun']}</td>
+    <td>{$row['chicken']}</td>
+    <td>{$row['beef']}</td>
+    <td>{$row['tofu']}</td>
+</tr>\n";
+
+}
+
+?>
+</table>
+</body>
+</html>
