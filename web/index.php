c0e5929947f57f57f722903e00335710893d439b
Kelly Remnant <remnankell@AD.ECS.VUW.AC.NZ>
2018-11-13 12:52:33 +1300

    16

--- web/index.php
+++ web/index.php
@@ -9,13 +9,13 @@
 </head>
 <body>
 <?php
-    $db_url = getenv("DATABASE_URL") ?: "postgres://user:pass@host:port/dbname";
+    $db_url = getenv("DATABASE_URL");
     echo "$db_url\n";
 
     $db = pg_connect($db_url);
     if($db) {echo "connected";} else {echo "not connected";}
 
-    $selectSql = "SELECT 1";
+    $selectSql = "SELECT * from recipes";
     $result =  pg_query($db, $selectSql);
 
     while ($row = pg_fetch_row($result)) {
