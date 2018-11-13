65a101d5c50588a19108bea8e55b72ae0c0334f8
Kelly Remnant <remnankell@AD.ECS.VUW.AC.NZ>
2018-11-13 13:03:50 +1300

    19

--- web/index.php
+++ web/index.php
@@ -10,32 +10,21 @@
 <body>
 <?php
     $db_url = getenv("DATABASE_URL");
-    //echo "$db_url\n";
+    echo "$db_url\n";
 
-    // $db = pg_connect($db_url);
-    // if($db) {echo "connected";} else {echo "not connected";}
-
-    // $selectSql = "SELECT * from recipes";
-    // $result =  pg_query($db, $selectSql);
-
-    // while ($row = pg_fetch_row($result)) {
-    //     var_dump($row);
-    // }
-
-    try {
-        $conn = new PDO($db_url);
-        // set the PDO error mode to exception
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        echo "Connected successfully"; 
-        }
-    catch(PDOException $e)
-        {
-        echo "Connection failed: " . $e->getMessage();
-        }
-
+    $db = pg_connect($db_url);
+    if($db) {echo "connected";} else {echo "not connected";}
 ?>
 <p>This is the main page</p>
 <p>burger name</p>
+<?php
+    $selectSql = "SELECT * from recipes";
+    $result =  pg_query($db, $selectSql);
+
+    while ($row = pg_fetch_row($result)) {
+        var_dump($row);
+    }
+?>
 
 </body>
 </html>
