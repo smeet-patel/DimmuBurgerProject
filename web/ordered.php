741e742917d4988160fee7c2f9cec3df00d274fd
Kelly Remnant <remnankell@AD.ECS.VUW.AC.NZ>
2018-11-13 16:59:02 +1300

    3

--- web/ordered.php
+++ web/ordered.php
@@ -16,6 +16,20 @@
 
         echo "Connected";
 
+        $stmt = $conn->prepare("INSERT INTO ingredients (paneer, tomato, lettuce)
+        VALUES (:paneer, :tomato, :lettuce)");
+        $stmt->bindParam(':paneer', $paneer);
+        $stmt->bindParam(':tomato', $tomato);
+        $stmt->bindParam(':lettuce', $lettuce);
+    
+        $paneer = $_POST['Paneer'];
+        $tomato = $_POST['Tomato'];
+        $lettuce = $_POST['Lettuce'];
+        $stmt->execute();
+        echo "New records created successfully";
+
+
+
     }catch(PDOException $e){
     echo "Error: " . $e->getMessage();
     }
