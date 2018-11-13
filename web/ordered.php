8a6fa34cdc597a21558384bb561b2900144ae253
Kelly Remnant <remnankell@AD.ECS.VUW.AC.NZ>
2018-11-14 11:54:42 +1300

    fet

--- web/ordered.php
+++ web/ordered.php
@@ -14,7 +14,7 @@
         ));
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-        echo "Connected";
+        // echo "Connected";
 
         $stmt = $conn->prepare("INSERT INTO recipes (burgerbun, junior, wrap, chicken, beef, falafel, tofu, paneer, tomato, lettuce, capsicum, onion, pineapple, carrot, avocado, pickles, cheddar, swiss, halloumi, tomatosauce, curry, italian, aioli, mayo)
         VALUES (:burgerbun, :junior, :wrap, :chicken, :beef, :falafel, :tofu, :paneer, :tomato, :lettuce, :capsicum, :onion, :pineapple, :carrot, :avocado, :pickles, :cheddar, :swiss, :halloumi, :tomatosauce, :curry, :italian, :aioli, :mayo)");
@@ -104,26 +104,7 @@
 
 
         $stmt->execute();
-        echo "New records created successfully";
-
-
-        $sql = 'SELECT MAX(burgername) FROM recipes';
-        $st = $pdo->prepare($sql);
-        $st->execute(['ordernumber' => $ordernumber]);
-        $post = $st->fetch();
-
-
-        $stm = $conn->prepare("INSERT INTO orders (ordernumber, burger, orderstate)
-        VALUES (:ordernumber, :burger, :orderstate)");
-        $stm->bindParam(':ordernumber', $ordernumber);
-        $stm->bindParam(':burger', $ordernumber);
-        $stm->bindParam(':orderstate', $orderstate);
-    
-        $orderstate = "new";
-
-        $stm->execute();
-        echo "Order: " + $ordernumber + " " + $orderstate;
-
+        // echo "New records created successfully";
 
     }catch(PDOException $e){
     echo "Error: " . $e->getMessage();
@@ -133,6 +114,63 @@
 
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<!-- Basic Page Needs
+			–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+	<meta charset="utf-8">
+	<title>Dimmu Burgers</title>
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<!-- Mobile Specific Metas
+			–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- CSS
+			–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+	<link rel="stylesheet" href="css/normalize.css">
+	<!-- <link rel="stylesheet" href="css/skeleton.css"> -->
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU"
+	 crossorigin="anonymous">
+	<!-- Favicon
+			–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+	<link rel="icon" type="images/png" href="images/DB.png">
+</head>
+
+<body>
+	<div class="nav1" id="mynav1">
+		<!--Adding My logo to the left and always showing -->
+		<a href="index.php">Dimmu Burger</a>
+		<!--Floating to the Left Side of the menu, the other pages-->
+		<a href="order.php">Order</a>
+		<!-- Shows menubutton when screen is small
+		<a href="javascript:void(0);" style="font-size:15px;" class="menubutton" onclick="myFunction()">&#9776;</a> -->
+	</div>
+	<br>
+	<!-- Primary Page Layout
+			–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+	<div class="containerWork">
+		<div id="mid">
+			<h1 id="bread" style="padding-bottom: 0.5em;">ORDER STATUS:</h1>
+            <?php
+
+                $stmt = $pdo->query('SELECT * FROM orders');
+                while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+                    echo $row['burger'] . '<br>';
+                }
+
+            ?>
+	    </div>
+	</div>
+	<script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+	<!-- <script src='javascript/bootstrap.min.js'></script> -->
+	<script src='javascript/jquery.min.js'></script>
+	<script src="javascript/script.js"></script>
+</body>
+
+</html>
 
 
 
