41cdace0b3904614ddd692fc409cb67ebf8a49cb
Kelly Remnant <remnankell@AD.ECS.VUW.AC.NZ>
2018-11-13 14:28:09 +1300

    2

--- web/index.php
+++ web/index.php
@@ -9,19 +9,32 @@
 </head>
 <body>
 <?php
-    $db_url = getenv("DATABASE_URL") ?: "postgres://user:pass@host:port/dbname";
-    echo "$db_url\n";
+    // $db_url = getenv("DATABASE_URL") ?: "postgres://user:pass@host:port/dbname";
+    // echo "$db_url\n";
 
-    $db = pg_connect($db_url);
-    if($db) {echo "connected";} else {echo "not connected";}
+    // $db = pg_connect($db_url);
+    // if($db) {echo "connected";} else {echo "not connected";}
 
-    $selectSql = "SELECT ingredient FROM ingredients";
-    $result =  pg_query($db, $selectSql);
+    // $selectSql = "SELECT ingredient FROM ingredients";
+    // $result =  pg_query($db, $selectSql);
 
-    while ($row = pg_fetch_row($result)) {
-        var_dump($row);
-    }
+    // while ($row = pg_fetch_row($result)) {
+    //     var_dump($row);
+    // }
+    $db = parse_url(getenv("DATABASE_URL"));
+
+    $pdo = new PDO("pgsql:" . sprintf(
+        "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+        $db["host"],
+        $db["port"],
+        $db["user"],
+        $db["pass"],
+        ltrim($db["path"], "/")
+    ));
+
+
 ?>
 
+<p>Testing...</p>
 </body>
 </html>
