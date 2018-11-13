9f7f631252249967ee7f4abf4d39ab1ebccc5e32
Kelly Remnant <remnankell@AD.ECS.VUW.AC.NZ>
2018-11-13 13:52:50 +1300

    24

--- web/index.php
+++ web/index.php
@@ -10,6 +10,7 @@
 <body>
 <?php
     $db = parse_url(getenv("DATABASE_URL"));
+    $db["path"] = ltrim($db["path"], "/");
     $dbconnect=mysqli_connect($db);
 
     if ($dbconnect->connect_error) {
