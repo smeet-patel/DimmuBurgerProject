6c9b4e54a3b34b32bdfaba5785e9c8baf0a2bcbf
Kelly Remnant <remnankell@AD.ECS.VUW.AC.NZ>
2018-11-15 12:35:22 +1300

    sa

--- web/javascript/script.js
+++ web/javascript/script.js
@@ -615,6 +615,7 @@ output12.innerHTML = slider12.value;
 slider12.oninput = function () {
   output12.innerHTML = this.value;
   if (this.value == 1) {
+    saucesdisplay();
     price12.innerHTML = "$" + parseFloat((-.1 * this.value) + 0.6).toFixed(2);
     pp12 = parseFloat((-.1 * this.value) + 0.6).toFixed(2);
   } else if (this.value == 2) {
