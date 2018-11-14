a31fd57ed255dd2bebfe8e6c90869713f842fade
Kelly Remnant <remnankell@AD.ECS.VUW.AC.NZ>
2018-11-14 22:35:17 +1300

    t

--- web/status.php
+++ web/status.php
@@ -2,7 +2,11 @@
 // Get Query String
     $stmt3 = $conn->query('SELECT MAX(ordernumber) AS ordernumber FROM orders');
 
+    $stat = "";
+
     while($row = $stmt3->fetch(PDO::FETCH_ASSOC)){
-        echo '<p>Your order number is <b>' . $row['ordernumber'] . '</b></p>';
+        $stat = 'Your order number is <b>' . $row['ordernumber'];
     }
+
+    echo $stat;
 ?>
