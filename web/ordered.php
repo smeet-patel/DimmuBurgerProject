181ae7a0b8285c5c3abc2a411f89221d329aa9ad
Kelly Remnant <remnankell@AD.ECS.VUW.AC.NZ>
2018-11-15 11:20:50 +1300

    iframe

--- web/ordered.php
+++ web/ordered.php
@@ -197,7 +197,13 @@
             ?>
             <div class="container">
                 <p style="font-size: 1.2em; line-height: 1.6em"><span id="output"></span></p>
-            </div>            
+            </div>  
+            <br>
+            <p style="text-align: center">Keep your order number handy
+                <br/>and pick your order from
+                <br/>Dimmu Burger, 139 Vivian St, Te Aro
+            </p>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2997.61412092318!2d174.77342531542297!3d-41.29550197927276!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6d38afd8f8caafa3%3A0xb62ac32a2a308cf7!2sVictoria+University+of+Wellington%2C+Faculty+of+Architecture+and+Design!5e0!3m2!1sen!2snz!4v1542228347739" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>          
         
 	    </div>
     </div>
